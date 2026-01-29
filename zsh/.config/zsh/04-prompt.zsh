6ec218c1cb56ba1650e6e7b076ac98d7dc1a8543
malechus <garrett@malechus.com>
2026-01-29 18:01:22 -0500

    updates prompt

--- zsh/.config/zsh/04-prompt.zsh
+++ zsh/.config/zsh/04-prompt.zsh
@@ -11,7 +11,7 @@ RPROMPT="%F{#F6C604}%*%f"
 
 # Display user@host and then PWD (depth of 4) on new line on left
 PS1="%F{#F6C604}%B%n@%m%b%f
-%F{#069494}%4~%f:"
+%F{#069494}%4~%#%f"
 
 # Insert empty line before each prompt, except first prompt
 precmd() {
