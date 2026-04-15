55b253f30b5ef172b64c8959298f5af06705e6e2
Garrett Kelly <garrett@malechus.com>
2026-04-15 17:48:18 -0400

    updates i3 and picom configs

--- i3/.config/i3/top.sh
+++ i3/.config/i3/top.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-2 --mode 1920x1080 --pos 3000x0 --rotate left --output DP-3 --mode None --pos 1080x0 --rotate normal
+xrandr --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 3000x0 --rotate left --output DP-2 --primary --mode 1920x1080 --pos 1080x0 --rotate normal --output DP-3 --mode 1920x1080 --pos 0x0 --rotate right
