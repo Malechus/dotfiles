dff77f0446dbcdee8f8b98d96cee4c8b79069f63
Garrett Kelly <garrett@malechus.com>
2026-04-17 10:37:40 -0400

    updates i3 display scripting

--- i3/.config/i3/bottom.sh
+++ i3/.config/i3/bottom.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 3000x0 --rotate left --output DP-2 --primary --mode 1920x1080 --pos 1080x840 --rotate normal --output DP-3 --mode 1920x1080 --pos 0x0 --rotate right
+if [ "$(hostname)" = "polyphemus" ]; then
+	xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 3014x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1080x840 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 0x0 --rotate right --output DP-5 --off
+fi
