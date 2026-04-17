66a2eff4a0ad6237bdb0d76f94632d245a9c9641
Garrett Kelly <garrett@malechus.com>
2026-04-17 15:48:34 -0400

    updates display scripts

--- i3/.config/i3/top.sh
+++ i3/.config/i3/top.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if [ "$(hostname)" = "polyphemus" ]; then
+if [ "$(hostnamectl --static)" = "polyphemus" ]; then
 	xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 3014x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1094x0 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 0x0 --rotate right --output DP-5 --off
 fi
