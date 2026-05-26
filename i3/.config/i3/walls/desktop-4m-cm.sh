1dd3945e217c014836e8b03af200b1484f772f2b
Garrett Kelly <garrett@malechus.com>
2026-05-25 23:10:37 -0400

    updates bar settings/colors, adds wallpapers

--- i3/.config/i3/walls/desktop-4m-cm.sh
+++ i3/.config/i3/walls/desktop-4m-cm.sh
@@ -2,4 +2,4 @@
 
 cd ~/.config/i3/walls
 
-feh --bg-fill horizontal/flowers-19.png horizontal/marin-eat-cat-mac.png vertical/marin-cat-mac.png vertical/maomao1.png --no-fehbg
+feh --bg-fill horizontal/flowers-19.png horizontal/marin-eat-cat-mac.png vertical/frieren3.png vertical/maomao1.png --no-fehbg
