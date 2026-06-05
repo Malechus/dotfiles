73938802cef501b5b70cf296df5c4649407eb212
Garrett Kelly <garrett@malechus.com>
2026-06-05 09:41:16 -0400

    adds autopairs to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,9 +3,13 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
+Plug('windwp/nvim-autopairs')
+
 Plug('catppuccin/nvim', { ['as'] = 'catppuccin' })
 
 vim.call('plug#end')
+
+require("nvim-autopairs").setup {}
 
 require("catppuccin").setup({
 	flavour = "macchiato",
