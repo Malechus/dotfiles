98945b659e53c4ea20a3d1fbf7288d5809e6887b
Garrett Kelly <garrett@malechus.com>
2026-06-05 11:59:37 -0400

    adds nvim-tree

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,14 +1,24 @@
 local vim = vim
 local Plug = vim.fn['plug#']
 
+-- Disable netrw, in favor of nvim-tree
+vim.g.loaded_netrw 		= 1
+vim.g.loaded_netrwPlugin 	= 1
+
+-- Install Plugins
 vim.call('plug#begin')
 
 Plug('windwp/nvim-autopairs')
 
 Plug('catppuccin/nvim', { ['as'] = 'catppuccin' })
 
+Plug('nvim-tree/nvim-web-devicons')
+
+Plug('nvim-tree/nvim-tree.lua')
+
 vim.call('plug#end')
 
+-- Setup Plugins
 require("nvim-autopairs").setup {}
 
 require("catppuccin").setup({
@@ -20,4 +30,14 @@ require("catppuccin").setup({
         },
 })
 
+require("nvim-tree").setup () 
+
+-- Keybindings
+local function map(m, k, v)
+	vim.keymap.set(m, k, v, { noremap = true, silent = true})
+end
+-- nvim-tree
+map("n", "<C-o>", ":NvimTreeToggle<CR>") --open file explorer
+
+-- Set theme
 vim.cmd.colorscheme "catppuccin"
